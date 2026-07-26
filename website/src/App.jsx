07e3d16c389e4e068fc0e645cf8466521bcb8d39
LaTanaDeiGatti <primianocarpinonemichele@gmail.com>
2026-07-26 08:44:29 +0000

    added

---
+++ website/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black">
+      <h1 className="text-5xl font-bold text-white">
+        N.O.V.A Security
+      </h1>
+    </div>
+  )
+}
+
+export default App
